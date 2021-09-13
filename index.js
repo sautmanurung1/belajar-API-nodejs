b8f0faf1b0dc94354132dfddf559c86c19cd3e3f
sautmanurung1 <smanurung360@gmail.com>
2021-09-13 20:56:52 +0700

    Belajar API Menggunakan Express JS

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const app = express();
-
 const productsRouters = require('../project/src/routers/products')
-
+const authRouters = require('../project/src/routers/auth');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use((req,res,next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
@@ -11,6 +11,7 @@ app.use((req,res,next) =>{
     next();
 })
 app.use('/v1/', productsRouters);
+app.use('/v1/auth', authRouters);
 app.use(() => {
     console.log('Welcome!');
 })
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,24 +1,25 @@
-exports.createProducts = (req, res, next) => {
-    console.log('request: ', req.body);
+exports.createProducts = (req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
     res.json({
         message: 'Create Products success',
         data:{
             id : 1, 
-            name : 'sari gandum enak',
-            price : 8000,
+            name : name,
+            price : price,
         }
     })
-    next();
 }
 
-exports.getALLProducts = (req, res, next) => {
+exports.getALLProducts = (req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
     res.json({
         message : "Get All Products success",
         data:{
             id : 1, 
-            name : 'sari gandum enak',
-            price : 8000,
+            name : name,
+            price : price,
         }
     })
-    next();
 }
